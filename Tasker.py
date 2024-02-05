cc519f64e3256169bdf90c0128c13fb7b9b0c4fa
Aakidul <157734003+Aakidul@users.noreply.github.com>
2024-02-05 15:36:43 +0530

    Create Tasker.py

---
+++ Tasker.py
@@ -0,0 +1,27 @@
+import os
+import time
+os.system("clear")
+
+print("IF TASKS ENTER IS DONE SO ENTER DONE")
+
+def main():
+    task = {}
+
+    while True:
+        user_input = input("ENTER DAILY TASK'S: ")
+
+        if user_input.lower() == "done":
+            break
+
+        if user_input.lower() == "show":
+            print(task.values())
+            continue
+
+        task.update({f"{len(task) + 1}": user_input})
+
+    print("Tasks entered:")
+    for key, value in task.items():
+        print(f"{key}: {value}")
+
+if __name__ == "__main__":
+    main()
